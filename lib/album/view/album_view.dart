bb468e2eb41b1d87be9af8bfaa4b1e4f07d876af
Ana Polo <anna-91_@hotmail.com>
2025-01-16 13:31:28 +0100

    feat: add albums ui

--- lib/album/view/album_view.dart
+++ lib/album/view/album_view.dart
@@ -15,29 +15,195 @@ class AlbumView extends StatelessWidget {
             child: CircularProgressIndicator(),
           )
         : isSuccess
-            ? const AlbumList()
+            ? const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  AlbumsHeader(),
+                  Expanded(
+                    child: AlbumsContent(),
+                  ),
+                ],
+              )
             : const Text('Failed to fetch albums');
   }
 }
 
-class AlbumList extends StatelessWidget {
-  const AlbumList({super.key});
+class AlbumsHeader extends StatelessWidget {
+  const AlbumsHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 16,
+        top: 16,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Taylor Swift',
+            style: textTheme.headlineLarge?.copyWith(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            'Discover her discography, from her first album to the latest one.',
+            style: textTheme.bodyMedium,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AlbumsContent extends StatelessWidget {
+  const AlbumsContent({super.key});
 
   @override
   Widget build(BuildContext context) {
     final albums = context.select((AlbumBloc bloc) => bloc.state.albums);
-    return ListView.builder(
+    return GridView.builder(
+      padding: const EdgeInsets.all(16),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
+      ),
       itemCount: albums.length,
       itemBuilder: (context, index) {
         final album = albums[index];
-        return ListTile(
-          title: Text(album.title),
-          subtitle: Text(album.releaseDate),
-          leading: album.coverAlbum != null
-              ? Image.network(album.coverAlbum!)
-              : null,
+        return AlbumItem(
+          title: album.title,
+          releaseDate: album.releaseDate,
+          coverAlbum: album.coverAlbum,
         );
       },
+    );
+  }
+}
+
+class AlbumItem extends StatelessWidget {
+  const AlbumItem({
+    required this.coverAlbum,
+    required this.title,
+    required this.releaseDate,
+    super.key,
+  });
+
+  final String? coverAlbum;
+  final String title;
+  final String releaseDate;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.hardEdge,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          AlbumImage(coverAlbum: coverAlbum),
+          const AlbumImageGradient(),
+          Positioned(
+            bottom: 4,
+            left: 8,
+            right: 8,
+            child: AlbumInfo(
+              title: title,
+              releaseDate: releaseDate,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AlbumInfo extends StatelessWidget {
+  const AlbumInfo({
+    required this.title,
+    required this.releaseDate,
+    super.key,
+  });
+
+  final String title;
+  final String releaseDate;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: textTheme.bodyLarge?.copyWith(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        Text(
+          releaseDate,
+          style: textTheme.bodySmall?.copyWith(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class AlbumImage extends StatelessWidget {
+  const AlbumImage({
+    required this.coverAlbum,
+    super.key,
+  });
+
+  final String? coverAlbum;
+
+  @override
+  Widget build(BuildContext context) {
+    if (coverAlbum != null) {
+      return Image.network(
+        coverAlbum!,
+        fit: BoxFit.cover,
+      );
+    } else {
+      return const SizedBox.shrink();
+    }
+  }
+}
+
+class AlbumImageGradient extends StatelessWidget {
+  const AlbumImageGradient({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.black.withValues(alpha: 0, red: 0, green: 0, blue: 0),
+            Colors.black.withValues(alpha: .5, red: 0, green: 0, blue: 0),
+            Colors.black.withValues(alpha: .5, red: 0, green: 0, blue: 0),
+          ],
+          stops: const [0, 0.5, 1.0],
+        ),
+      ),
     );
   }
 }
